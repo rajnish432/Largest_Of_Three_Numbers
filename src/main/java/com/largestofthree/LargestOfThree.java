2b84abf75b21d31d9f932831ecb853a59b976d2f
rajnish432 <rajnish.kahar1996@gmail.com>
2020-02-28 16:26:24 +0530

    REFACTORED_1: UC3_Used Generics To Replace Three Methods by One

--- src/main/java/com/largestofthree/LargestOfThree.java
+++ src/main/java/com/largestofthree/LargestOfThree.java
@@ -2,8 +2,8 @@ package com.largestofthree;
 
 public class LargestOfThree {
 
-    public Integer getMaximum(Integer num1, Integer num2, Integer num3) {
-        Integer max=num1;
+    public <E extends Comparable> E getMaximum(E num1, E num2, E num3) {
+        E max=num1;
         if(num2.compareTo(max)>0)
         {
             max=num2;
@@ -11,32 +11,6 @@ public class LargestOfThree {
         if(num3.compareTo(max)>0)
         {
             max=num3;
-        }
-        return max;
-    }
-
-    public Float getFloatMaximum(Float n1,Float n2,Float n3) {
-        Float max=n1;
-        if(n2.compareTo(max)>0)
-        {
-            max=n2;
-        }
-        if (n3.compareTo(max)>0)
-        {
-            max=n3;
-        }
-        return max;
-    }
-
-    public String getStringMaximum(String a,String b,String c) {
-        String max=a;
-        if (b.compareTo(max)>0)
-        {
-            max=b;
-        }
-        if (c.compareTo(max)>0)
-        {
-            max=c;
         }
         return max;
     }
--- src/test/java/LargestOfThreeTest.java
+++ src/test/java/LargestOfThreeTest.java
@@ -25,36 +25,36 @@ public class LargestOfThreeTest {
 
     @Test
     public void givenThreeFloatValues_IfProper_returnFirstMaximum() {
-        float result= largestOfThree.getFloatMaximum(15.0f,10.0f,3.0f);
+        float result= largestOfThree.getMaximum(15.0f,10.0f,3.0f);
         Assert.assertEquals(15.0f,result,0.0);
     }
 
     @Test
     public void givenThreeFloatValues_IfProper_returnSecondMaximum() {
-        float result= largestOfThree.getFloatMaximum(10.0f,15.0f,3.0f);
+        float result= largestOfThree.getMaximum(10.0f,15.0f,3.0f);
         Assert.assertEquals(15.0f,result,0.0);
     }
 
     @Test
     public void givenThreeFloatValues_IfProper_returnThirdMaximum() {
-        float result= largestOfThree.getFloatMaximum(3.0f,10.0f,15.0f);
+        float result= largestOfThree.getMaximum(3.0f,10.0f,15.0f);
         Assert.assertEquals(15,result,0.0);
     }
     @Test
     public void givenThreeStrings_IfProper_returnFirstMaximum() {
-        String result=largestOfThree.getStringMaximum("Peach","Apple","Banana");
+        String result=largestOfThree.getMaximum("Peach","Apple","Banana");
         Assert.assertEquals("Peach",result);
     }
 
     @Test
     public void givenThreeStrings_IfProper_returnSecondMaximum() {
-        String result=largestOfThree.getStringMaximum("Apple","Peach","Banana");
+        String result=largestOfThree.getMaximum("Apple","Peach","Banana");
         Assert.assertEquals("Peach",result);
     }
 
     @Test
     public void givenThreeStrings_IfProper_returnThirdtMaximum() {
-        String result=largestOfThree.getStringMaximum("Apple","Banana","Peach");
+        String result=largestOfThree.getMaximum("Apple","Banana","Peach");
         Assert.assertEquals("Peach",result);
     }
 }
