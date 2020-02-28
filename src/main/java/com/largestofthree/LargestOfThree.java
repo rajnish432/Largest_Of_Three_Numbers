f5510ea29bbaaf95a70ddd19fbf5fe80a16f0dfe
rajnish432 <rajnish.kahar1996@gmail.com>
2020-02-28 15:10:48 +0530

    ADDED: Welcome Message

---
+++ src/main/java/com/largestofthree/LargestOfThree.java
@@ -0,0 +1,26 @@
+package com.largestofthree;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Test Maximum");
+        LargestOfThree largestOfThree=new LargestOfThree();
+        //Integer Objects
+        Integer num1=0;
+        Integer num2 =0;
+        Integer num3=0;
+        largestOfThree.getMaximum(num1,num2,num3);
+    }
+
+    public int getMaximum(Integer num1, Integer num2, Integer num3) {
+        Integer max=num1;
+        if(num2.compareTo(max)>0)
+        {
+            max=num2;
+        }
+        if(num3.compareTo(max)>0)
+        {
+            max=num3;
+        }
+        return max;
+    }
+}
---
+++ src/test/java/LargestOfThreeTest.java
@@ -0,0 +1,24 @@
+import com.largestofthree.LargestOfThree;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LargestOfThreeTest {
+    LargestOfThree largestOfThree=new LargestOfThree();
+    @Test
+    public  void givenThreeNumber_IfProper_returnFirstMaximum() {
+        int result=largestOfThree.getMaximum(15,10,3);
+        Assert.assertEquals(15,result);
+    }
+
+    @Test
+    public void givenThreeNumber_IfProper_returnSecondMaximum() {
+        int result=largestOfThree.getMaximum(10,15,3);
+        Assert.assertEquals(15,result);
+    }
+
+    @Test
+    public void givenThreeNumber_IfProper_returnThirdMaximum() {
+        int result=largestOfThree.getMaximum(10,3,15);
+        Assert.assertEquals(15,result);
+    }
+}
